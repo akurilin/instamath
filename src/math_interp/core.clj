5111fc73bbec74153ed34030f5e0b6fe954dc21f
Alexandr Kurilin <alex@kurilin.net>
2013-11-30 18:01:26 -0800

    Adds double, coercion and generalizes expression

--- src/math_interp/core.clj
+++ src/math_interp/core.clj
@@ -1,10 +1,35 @@
 (ns math-interp.core
   (:require [instaparse.core :as insta]))
 
-; (def as-and-bs
-;   (insta/parser
-;     "S = AB*
-;      AB = A B
-;      A = 'a'+
-;      B = 'b'+"))
+; Questions:
+; * whitespace preprocessing vs whitespace in grammar
 
+(def math (insta/parser (slurp "grammar.ebnf")))
+
+(defn pick-op
+  [op]
+  (case op
+    "+" +
+    )
+  )
+
+(def transform-options
+  {:integer #(Long/parseLong %)
+   :floating #(Double/parseDouble %)
+   :number identity
+   :op pick-op
+   :operation (fn [a op b] (op a b))
+   :expr identity})
+
+(defn math-eval
+  [source & options]
+  (->> (clojure.string/replace source #"\s+" "")
+       ((fn [x] (if (:show-ambiguity (first options))
+                 (insta/parses math x)
+                 (insta/parse math x))))
+       ((fn [tree] (do (clojure.pprint/pprint tree) tree)))
+       (insta/transform transform-options)
+       ((fn [res] (do (clojure.pprint/pprint res) res)))))
+
+; (math-eval "1+1")
+; (math-eval "1")
--- test/math_interp/core_test.clj
+++ test/math_interp/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [math-interp.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest integers
+  (is (= 1 (math-eval "1")))
+  (is (= java.lang.Long (type (math-eval "1")))))
+
+(deftest doubles
+  (let [res (math-eval "0.333")]
+    (is (= 0.333 res))
+    (is (= java.lang.Double (type res)))))
+
+(deftest addition
+  (is (= 2 (math-eval "1 + 1"))))
+
+(deftest coercion
+  (let [res (math-eval "1 + 0.55")]
+    (is (== (+ 1 0.55) res))
+    (is (= java.lang.Double (type res)))))
+
+(deftest whitespace
+  (is (= 2 (math-eval "1+1")))
+  (is (= 2 (math-eval "   1    +    1  "))))
+
+(deftest addition-multiple-operands)
